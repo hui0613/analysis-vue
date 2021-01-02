8400d4039fba105ffcfd3fdb80b2fab2aef2d629
hui-613 <2516772557@qq.com>
2021-01-02 22:11:07 +0800

    init

---
+++ vue/src/core/index.js
@@ -0,0 +1,29 @@
+import Vue from "./instance/index";
+import { initGlobalAPI } from "./global-api/index";
+import { isServerRendering } from "core/util/env";
+import { FunctionalRenderContext } from "core/vdom/create-functional-component";
+
+// 初始化全局 API
+initGlobalAPI(Vue);
+
+// 设置服务端渲染
+Object.defineProperty(Vue.prototype, "$isServer", {
+  get: isServerRendering,
+});
+
+// 设置 ssr 配置
+Object.defineProperty(Vue.prototype, "$ssrContext", {
+  get() {
+    /* istanbul ignore next */
+    return this.$vnode && this.$vnode.ssrContext;
+  },
+});
+
+// expose FunctionalRenderContext for ssr runtime helper installation
+Object.defineProperty(Vue, "FunctionalRenderContext", {
+  value: FunctionalRenderContext,
+});
+
+Vue.version = "__VERSION__";
+
+export default Vue;
---
+++ vue/src/core/instance/index.js
@@ -0,0 +1,23 @@
+import { initMixin } from "./init";
+import { stateMixin } from "./state";
+import { renderMixin } from "./render";
+import { eventsMixin } from "./events";
+import { lifecycleMixin } from "./lifecycle";
+import { warn } from "../util/index";
+
+// 定义 Vue 构造函数
+function Vue(options) {
+  // 通过判断 Vue 调用对象来确保只能使用 new 关键字创建 Vue 实例，不可直接使用 Vue() 的形式
+  if (process.env.NODE_ENV !== "production" && !(this instanceof Vue)) {
+    warn("Vue is a constructor and should be called with the `new` keyword");
+  }
+  this._init(options);
+}
+
+initMixin(Vue);
+stateMixin(Vue);
+eventsMixin(Vue);
+lifecycleMixin(Vue);
+renderMixin(Vue);
+
+export default Vue;
